173d1f7d9b64888fb29fef8fe2013cf1373ff3a8
Gregory Covington <greg@virulantconsulting.com>
2017-02-12 22:49:56 -0600

    Uncommented the other ships in globvars

--- globvars.py
+++ globvars.py
@@ -1,8 +1,8 @@
 SHIP_INFO = [
-    # ("Aircraft Carrier", 5)
-    # ("Battleship", 4),
-    # ("Submarine", 3),
-    # ("Cruiser", 3),
+    ("Aircraft Carrier", 5)
+    ("Battleship", 4),
+    ("Submarine", 3),
+    ("Cruiser", 3),
     ("Patrol Boat", 2)
 ]
 
